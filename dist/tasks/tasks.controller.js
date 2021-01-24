d8197acd9d14be09facfb57ae88e237e80807aea
AmrAkmal <amrakmal97@gmail.com>
2021-01-24 19:53:05 +0200

    needed resources

--- dist/tasks/tasks.controller.js
+++ dist/tasks/tasks.controller.js
@@ -5,16 +5,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppModule = void 0;
-const common_1 = require("@nestjs/common");
-const tasks_module_1 = require("./tasks/tasks.module");
-let AppModule = class AppModule {
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-AppModule = __decorate([
-    common_1.Module({
-        imports: [tasks_module_1.TasksModule],
-    })
-], AppModule);
-exports.AppModule = AppModule;
-//# sourceMappingURL=app.module.js.map
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TasksController = void 0;
+const common_1 = require("@nestjs/common");
+const tasks_service_1 = require("./tasks.service");
+let TasksController = class TasksController {
+    constructor(tasksService) {
+        this.tasksService = tasksService;
+    }
+};
+TasksController = __decorate([
+    common_1.Controller('tasks'),
+    __metadata("design:paramtypes", [tasks_service_1.TasksService])
+], TasksController);
+exports.TasksController = TasksController;
+//# sourceMappingURL=tasks.controller.js.map
--- dist/tasks/tasks.service.js
+++ dist/tasks/tasks.service.js
@@ -6,15 +6,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppModule = void 0;
+exports.TasksService = void 0;
 const common_1 = require("@nestjs/common");
-const tasks_module_1 = require("./tasks/tasks.module");
-let AppModule = class AppModule {
+let TasksService = class TasksService {
 };
-AppModule = __decorate([
-    common_1.Module({
-        imports: [tasks_module_1.TasksModule],
-    })
-], AppModule);
-exports.AppModule = AppModule;
-//# sourceMappingURL=app.module.js.map
+TasksService = __decorate([
+    common_1.Injectable()
+], TasksService);
+exports.TasksService = TasksService;
+//# sourceMappingURL=tasks.service.js.map
